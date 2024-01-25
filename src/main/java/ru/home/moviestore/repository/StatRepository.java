ec6fd58f73e78d457ccca92c4aa08b0309e43cff
Iogann1978 <iogann1978@gmail.com>
2024-01-25 23:42:51 +0300

    add movies

--- src/main/java/ru/home/moviestore/repository/StatRepository.java
+++ src/main/java/ru/home/moviestore/repository/StatRepository.java
@@ -11,12 +11,15 @@ public interface StatRepository extends JpaRepository<Stat, Long> {
             "COUNT(mp.id)," +
             "COUNT(DISTINCT m1.id)," +
             "COUNT(DISTINCT m2.id)," +
-            "SUM(CASE WHEN mp.role = ACTOR THEN 1 ELSE 0 END)," +
-            "SUM(CASE WHEN mp.role = DIRECTOR THEN 1 ELSE 0 END)," +
-            "SUM(CASE WHEN mp.role = COMPOSER THEN 1 ELSE 0 END)," +
+            "COUNT(DISTINCT p1.id)," +
+            "COUNT(DISTINCT p2.id)," +
+            "COUNT(DISTINCT p3.id)," +
             "COUNT(DISTINCT mp.movieId)," +
             "COUNT(DISTINCT mp.personId))" +
             "FROM MoviePerson mp LEFT JOIN Movie m1 ON mp.movieId = m1.id AND m1.serial = false " +
-    "LEFT JOIN Movie m2 ON mp.movieId = m2.id AND m2.serial = true")
+            "LEFT JOIN Movie m2 ON mp.movieId = m2.id AND m2.serial = true " +
+            "LEFT JOIN Person p1 ON mp.personId = p1.id AND mp.role = ACTOR " +
+            "LEFT JOIN Person p2 ON mp.personId = p2.id AND mp.role = DIRECTOR "+
+            "LEFT JOIN Person p3 ON mp.personId = p3.id AND mp.role = COMPOSER")
     Stat getStat();
 }
