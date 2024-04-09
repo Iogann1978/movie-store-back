a0a183f6ed837724ccf9d951ce155c2497860260
Iogann1978 <iogann1978@gmail.com>
2024-04-09 23:52:58 +0300

    bug fix

--- src/main/java/ru/home/moviestore/mapper/StatMapper.java
+++ src/main/java/ru/home/moviestore/mapper/StatMapper.java
@@ -8,28 +8,28 @@ import ru.home.moviestore.model.Stat;
 public class StatMapper {
     public StatDto entityToDto(Stat entity, String bests) {
         return StatDto.builder()
-                .moviesCount(entity.getMoviesCount())
-                .seriesCount(entity.getSeriesCount())
-                .actorsCount(entity.getActorsCount())
-                .directorsCount(entity.getDirectorsCount())
-                .composersCount(entity.getComposersCount())
-                .totalMoviesCount(entity.getTotalMoviesCount())
-                .totalPersonsCount(entity.getTotalPersonsCount())
-                .bests(bests)
-                .build();
+            .moviesCount(entity.getMoviesCount())
+            .seriesCount(entity.getSeriesCount())
+            .actorsCount(entity.getActorsCount())
+            .directorsCount(entity.getDirectorsCount())
+            .composersCount(entity.getComposersCount())
+            .totalMoviesCount(entity.getTotalMoviesCount())
+            .totalPersonsCount(entity.getTotalPersonsCount())
+            .bests(bests)
+            .build();
     }
 
     public StatDto entityToDto(Stat entity) {
         return StatDto.builder()
-                .id(entity.getId())
-                .moviesCount(entity.getMoviesCount())
-                .seriesCount(entity.getSeriesCount())
-                .actorsCount(entity.getActorsCount())
-                .directorsCount(entity.getDirectorsCount())
-                .composersCount(entity.getComposersCount())
-                .totalMoviesCount(entity.getTotalMoviesCount())
-                .totalPersonsCount(entity.getTotalPersonsCount())
-                .bests(entity.getBests())
-                .build();
+            .id(entity.getId())
+            .moviesCount(entity.getMoviesCount())
+            .seriesCount(entity.getSeriesCount())
+            .actorsCount(entity.getActorsCount())
+            .directorsCount(entity.getDirectorsCount())
+            .composersCount(entity.getComposersCount())
+            .totalMoviesCount(entity.getTotalMoviesCount())
+            .totalPersonsCount(entity.getTotalPersonsCount())
+            .bests(entity.getBests())
+            .build();
     }
 }
