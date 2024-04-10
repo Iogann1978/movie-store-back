4f433637622a952b0e59fb445264a1080b1301d7
Iogann1978 <iogann1978@gmail.com>
2024-04-10 23:27:59 +0300

    bug fix

--- src/main/java/ru/home/moviestore/mapper/MovieMapper.java
+++ src/main/java/ru/home/moviestore/mapper/MovieMapper.java
@@ -12,62 +12,59 @@ import ru.home.moviestore.model.Tag;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @UtilityClass
 public class MovieMapper {
     public Movie dtoToEntity(MovieDto dto) {
         return Movie.builder()
-                .id(dto.getId())
-                .title(dto.getTitle())
-                .originTitle(Optional.ofNullable(dto.getOriginTitle()).orElseGet(dto::getTitle))
-                .externalRating(dto.getExternalRating())
-                .internalRating(dto.getInternalRating())
-                .duration(dto.getDuration())
-                .releaseYear(dto.getYear())
-                .serial(dto.getSerial())
-                .state(Movie.State.values()[Integer.valueOf(dto.getState())])
-                .countries(getContries(dto.getCountries()))
-                .tags(getTags(dto.getTags()))
-                .descripts(getDescripts(dto.getDescripts()))
-                .build();
+            .id(dto.getId())
+            .title(dto.getTitle())
+            .originTitle(Optional.ofNullable(dto.getOriginTitle()).orElseGet(dto::getTitle))
+            .externalRating(dto.getExternalRating())
+            .internalRating(dto.getInternalRating())
+            .duration(dto.getDuration())
+            .releaseYear(dto.getYear())
+            .serial(dto.getSerial())
+            .state(Movie.State.values()[Integer.valueOf(dto.getState())])
+            .countries(getContries(dto.getCountries()))
+            .tags(getTags(dto.getTags()))
+            .descripts(getDescripts(dto.getDescripts()))
+            .build();
     }
 
     public MovieDto entityToDto(Movie entity) {
         return MovieDto.builder()
-                .id(entity.getId())
-                .title(entity.getTitle())
-                .originTitle(entity.getOriginTitle())
-                .externalRating(entity.getExternalRating())
-                .internalRating(entity.getInternalRating())
-                .serial(entity.getSerial())
-                .duration(entity.getDuration())
-                .year(entity.getReleaseYear())
-                .state(String.valueOf(entity.getState().ordinal()))
-                .countries(getContriesDto(entity.getCountries()))
-                .tags(getTagsDto(entity.getTags()))
-                .descripts(getDescriptsDto(entity.getDescripts()))
-                .build();
+            .id(entity.getId())
+            .title(entity.getTitle())
+            .originTitle(entity.getOriginTitle())
+            .externalRating(entity.getExternalRating())
+            .internalRating(entity.getInternalRating())
+            .serial(entity.getSerial())
+            .duration(entity.getDuration())
+            .year(entity.getReleaseYear())
+            .state(String.valueOf(entity.getState().ordinal()))
+            .countries(getContriesDto(entity.getCountries()))
+            .tags(getTagsDto(entity.getTags()))
+            .descripts(getDescriptsDto(entity.getDescripts()))
+            .build();
     }
 
     public MovieDto fromFilm(Film film) {
         return MovieDto.builder()
-                .id(film.getKinopoiskId())
-                .title(film.getNameRu())
-                .originTitle(getOriginalName(film))
-                .year(Optional.ofNullable(film.getYear()).map(Long::intValue).orElse(0))
-                .serial(film.getSerial())
-                .externalRating(getRating(film))
-                .duration(Optional.ofNullable(film.getFilmLength()).map(Long::intValue).orElse(0))
-                .tags(getTags(film.getGenres()))
-                .countries(getCountries(film.getCountries()))
-                .descripts(getDescript(film))
-                .state("0")
-                .build();
+            .id(film.getKinopoiskId())
+            .title(film.getNameRu())
+            .originTitle(getOriginalName(film))
+            .year(Optional.ofNullable(film.getYear()).map(Long::intValue).orElse(0))
+            .serial(film.getSerial())
+            .externalRating(getRating(film))
+            .duration(Optional.ofNullable(film.getFilmLength()).map(Long::intValue).orElse(0))
+            .tags(getTags(film.getGenres()))
+            .countries(getCountries(film.getCountries()))
+            .descripts(getDescript(film))
+            .state("0")
+            .build();
     }
 
     private Set<String> getContriesDto(Set<Country> countries) {
@@ -80,14 +77,14 @@ public class MovieMapper {
 
     private Set<Country> getContries(Set<String> countries) {
         return countries.stream()
-                .map(country -> Country.builder().id(country.toUpperCase()).name(country).build())
-                .collect(Collectors.toSet());
+            .map(country -> Country.builder().id(country.toUpperCase()).name(country).build())
+            .collect(Collectors.toSet());
     }
 
     private Set<Tag> getTags(Set<String> tags) {
         return tags.stream()
-                .map(tag -> Tag.builder().id(tag.toUpperCase()).name(tag).build())
-                .collect(Collectors.toSet());
+            .map(tag -> Tag.builder().id(tag.toUpperCase()).name(tag).build())
+            .collect(Collectors.toSet());
     }
 
     private Set<String> getTags(List<Genre> genres) {
@@ -100,13 +97,16 @@ public class MovieMapper {
 
     private Integer getRating(Film film) {
         return Optional.ofNullable(film.getRatingKinopoisk())
-                .map(rating -> rating.divide(BigDecimal.valueOf(2.0d), RoundingMode.HALF_UP))
-                .map(BigDecimal::intValue)
-                .orElse(0);
+            .map(rating -> rating.divide(BigDecimal.valueOf(2.0d), RoundingMode.HALF_UP))
+            .map(BigDecimal::intValue)
+            .orElse(0);
     }
 
     private Set<DescriptDto> getDescriptsDto(Set<Descript> descripts) {
-        return descripts.stream().map(DescriptMapper::entityToDto).collect(Collectors.toSet());
+        return descripts.stream()
+            .sorted(Comparator.comparing(Descript::getId))
+            .map(DescriptMapper::entityToDto)
+            .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
     private Set<Descript> getDescripts(Set<DescriptDto> descripts) {
